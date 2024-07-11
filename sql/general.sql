cbe76a3dfcc3422c9b48bec4972710f03ae8819e
Thunderkitten <83287471+thunderpawz@users.noreply.github.com>
2024-07-11 16:23:59 -0400

    Update general.sql

--- sql/general.sql
+++ sql/general.sql
@@ -1,1 +1,10 @@
+--MSSQL Search Table Query
+SELECT so.name as object, so.type, sc.* 
+FROM sysobjects SO 
+JOIN syscolumns sc on sc.id=so.id 
+WHERE so.type ='u'--in ('u','v') –- U=table, v=view 
+and So.name --so.name is table name 
+--Sc.name –- sc.name is column name 
+like '%{insert search string}%' 
+order by 1 
 
